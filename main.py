6918f93164de0df42ae6bbd94a52681050f77882
BKonstantin <costa.spy@yandex.ru>
2021-03-14 12:28:47 +0300

    1

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ con = sqlite3.connect("bd.db")
 cur = con.cursor()
 a = cur.execute("""SELECT * FROM products""")
 print(a)
-print("1с говно")
+print("1с говнbot")
 
 
 # Press Shift+F10 to execute it or replace it with your code.
