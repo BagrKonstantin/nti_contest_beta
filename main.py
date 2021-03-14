c94b619db2f278f2a1c5dbe648873411f058a171
Abusswalker <af55748@gmail.com>
2021-03-14 12:27:17 +0300

    Ленин - гриб

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import sqlite3
 
 con = sqlite3.connect("bd.db")
 cur = con.cursor()
-a = cur.execute("""SELECT * FROM products WHERE title""")
+a = cur.execute("""SELECT * FROM products""")
 print(a)
 print("1с говно")
 
